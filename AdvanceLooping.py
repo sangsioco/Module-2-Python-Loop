4321b6a4cce10899119569f4f7353010c2cf50f1
sangsioco <sangsioco@gmail.com>
2024-05-23 18:36:19 -0700

    completing selective dj task

---
+++ AdvanceLooping.py
@@ -0,0 +1,20 @@
+# Task 1 Selective DJ
+genres = ['Jazz', 'Rock', 'Hip-hop', 'Classical']
+
+selected_genres = genres[1:4]
+
+for genre in selected_genres:
+    print(genre)
+
+# Task 2 One-Liner band
+genres = ['Jazz', 'Rock', 'Hip-hop', 'Classical']
+
+genres_with_music = [genre + " Music" for genre in genres]
+
+print(genres_with_music)
+
+# Task 3 Numerical Beats
+
+for i in range(10, 0, -1):
+    print(i)
+print("The beat drops now!")
