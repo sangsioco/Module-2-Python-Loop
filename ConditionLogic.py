726af8e850cbd8c1fc4caf1324fa350f50bdc7cb
sangsioco <sangsioco@gmail.com>
2024-05-23 18:08:38 -0700

    loop condiditonal logic task

---
+++ ConditionLogic.py
@@ -0,0 +1,18 @@
+#Task 1 While loop condition is always true
+
+
+counter = 0  
+
+while True:  
+    print("Let the game begin!")  
+    counter += 1  
+    if counter == 5:  
+        break  
+
+# Task 2
+
+counter = 0
+
+while counter < 5:
+    counter += 1
+    print("Let the game begin")
